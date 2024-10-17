95bd2ecca9675c7d9a36a495681ba8311c1fbbac
sarvadandge29 <sarvadandge@gmail.com>
2024-10-17 09:00:37 +0530

    Update gitignore

--- env.js
+++
@@ -1,7 +0,0 @@
-export const endpoint= "https://cloud.appwrite.io/v1"
-export const platform= "com.sarvad.hostelFinder"
-export const projectId= "670aa46e0033db9b8723"
-export const databaseId= "670aa4c70011b6fd70d2" // Correcting the typo
-export const userCollectionId= "670aa4cd00271b545344"
-export const hostelCollectionId= "670aa684002aadaea9be"
-export const storageId= "670aa77200163d39e7b6"
