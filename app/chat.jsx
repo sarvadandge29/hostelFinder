af045f1de8d9b7af952fdc4f2d96973db4a19ac3
sarvadandge29 <sarvadandge@gmail.com>
2024-11-01 02:32:01 +0530

    "Updated Chat component to use ImageBackground, added renderBubble and renderTime functions, and imported wallpaper image"

--- app/chat.jsx
+++ app/chat.jsx
@@ -1,12 +1,12 @@
-import { View, Text, StatusBar, Image, TouchableOpacity, SafeAreaView } from 'react-native'
+import { View, Text, StatusBar, Image, TouchableOpacity, SafeAreaView, ImageBackground } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { databaseId, databases, messagesCollectionId } from '../lib/appwrite'
 import { router, useLocalSearchParams } from 'expo-router'
-import { icons } from '../constants'
+import { icons, images } from '../constants'
 import { useGlobalContext } from '../context/GlobalProvider';
 import { ID, Query } from 'react-native-appwrite'
-import { GiftedChat, InputToolbar } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 
 const Chat = () => {
     const { user } = useGlobalContext();
@@ -86,42 +86,89 @@ const Chat = () => {
         />
     );
 
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                textStyle={{
+                    left: {
+                        color: '#000'
+                    },
+                    right: {
+                        color: '#000'
+                    },
+                }}
+                wrapperStyle={{
+                    left: {
+                        backgroundColor: '#fff',
+                    },
+                    right: {
+                        backgroundColor: '#8DE0E0',
+                    },
+                }}
+            />
+        )
+    };
+
+    const renderTime = (props) => {
+        return (
+            <View style={{ marginLeft: 5, marginRight: 5, marginBottom: 3 }}>
+                <Text
+                    style={{
+                        fontSize: 10,
+                        color: '#000',
+                        opacity: 0.8, 
+                    }}
+                >
+                    {props.currentMessage.createdAt.toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    })}
+                </Text>
+            </View>
+        );
+    };
+
     const handlePress = () => {
         const route = user?.isAdmin ? 'adminChatTab' : 'userChatTab';
         router.push({ pathname: route });
     };
     return (
         <GestureHandlerRootView className="flex-1">
-            <SafeAreaView className="flex-1 bg-primary">
-                <View className="flex-row px-2 pt-10">
-                    <TouchableOpacity onPress={handlePress}>
+            <ImageBackground className="flex-1" source={images.wallpaper}>
+                <SafeAreaView className="flex-1">
+                    <View className="flex-row px-2 pt-1">
+                        <TouchableOpacity onPress={handlePress}>
+                            <Image
+                                source={icons.arrow_back}
+                                className="w-9 h-9 mt-1"
+                                resizeMode='contain'
+                            />
+                        </TouchableOpacity>
                         <Image
-                            source={icons.arrow_back}
-                            className="w-9 h-9 mt-1"
-                            resizeMode='contain'
+                            source={{ uri: avatar }}
+                            className="w-10 h-10 rounded-full mr-4"
                         />
-                    </TouchableOpacity>
-                    <Image
-                        source={{ uri: avatar }}
-                        className="w-10 h-10 rounded-full mr-4"
+                        <Text className="text-white text-2xl pt-1">{nameParam}</Text>
+                    </View>
+                    <GiftedChat
+                        messages={messages}
+                        onSend={onSend}
+                        user={{
+                            _id: user.$id,
+                            name: user.name,
+                            avatar: user.avatar,
+                        }}
+                        placeholder="Message"
+                        alwaysShowSend
+                        renderInputToolbar={renderInputToolbar}
+                        inverted={true}
+                        renderBubble={renderBubble}
+                        renderTime={renderTime}
                     />
-                    <Text className="text-white text-2xl pt-1">{nameParam}</Text>
-                </View>
-                <GiftedChat
-                    messages={messages}
-                    onSend={onSend}
-                    user={{
-                        _id: user.$id,
-                        name: user.name,
-                        avatar: user.avatar,
-                    }}
-                    placeholder="Type a message"
-                    alwaysShowSend
-                    renderInputToolbar={renderInputToolbar}
-                    inverted={true}
-                />
-                <StatusBar backgroundColor="#161622" style="light" />
-            </SafeAreaView>
+                    <StatusBar backgroundColor="#161622" style="light" />
+                </SafeAreaView>
+            </ImageBackground>
         </GestureHandlerRootView>
     );
 };
--- constants/images.js
+++ constants/images.js
@@ -2,5 +2,6 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import wallpaper from "../assets/images/wallpaper.jpg"
 
-export default { path, logo, logoSmall, empty };
+export default { path, logo, logoSmall, empty, wallpaper };
